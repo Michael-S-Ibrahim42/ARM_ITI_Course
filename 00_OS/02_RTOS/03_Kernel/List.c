c84e1134de323fa968af7a6d99adffc6c2228271
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-05-30 16:54:16 +0200

    Add list module

--- 00_OS/02_RTOS/03_Kernel/List.c
+++ 00_OS/02_RTOS/03_Kernel/List.c
@@ -15,7 +15,71 @@
 /* /////////////////////////////// Interface Fns //////////////////////// */
 void List_vidInsert(strTCB_t** Copy_ppstrList, strTCB_t* Copy_pstrNode, enuOrderType_t Copy_enuOrderType)
 {
-  
+  strTCB_t* Loc_pstrTraverser = *Copy_ppstrList;
+  if((Copy_ppstrList != NULL) && (Copy_pstrNode != NULL))    
+  {
+    if(*Copy_ppstrList == NULL) /* The list is empty */
+    {
+      *Copy_ppstrList = Copy_pstrNode;
+      Copy_pstrNode->Next = NULL;
+      Copy_pstrNode->Prev = NULL;
+    }/* if */
+    else /* We have at least one node in the list */
+    {
+      if(Copy_enuOrderType == enuOrderType_Pri)
+      {
+        if(Copy_pstrNode->TaskPri < ((*Copy_ppstrList)->TaskPri))
+        {
+          Copy_pstrNode->Next = *Copy_ppstrList;
+          Copy_pstrNode->Prev = NULL;
+          (*Copy_ppstrList)->Prev = Copy_pstrNode;
+          *Copy_ppstrList = Copy_pstrNode;
+        }/* if */
+        else
+        {
+          while((Loc_pstrTraverser->Next != NULL) && (Loc_pstrTraverser->Next->TaskPri < Copy_pstrNode->TaskPri))
+          {
+            Loc_pstrTraverser = Loc_pstrTraverser->Next;
+          }/* while */
+          Copy_pstrNode->Next = Loc_pstrTraverser->Next;
+          Copy_pstrNode->Prev = Loc_pstrTraverser;
+          Loc_pstrTraverser->Next = Copy_pstrNode;
+          if(Copy_pstrNode->Next != NULL)
+          {
+            Copy_pstrNode->Next->Prev = Copy_pstrNode;
+          }/* if */
+        }/* else */
+      }/* if */
+      else if(Copy_enuOrderType == enuOrderType_Tick)
+      {
+        if(Copy_pstrNode->WaitTicks < ((*Copy_ppstrList)->WaitTicks))
+        {
+          Copy_pstrNode->Next = *Copy_ppstrList;
+          Copy_pstrNode->Prev = NULL;
+          (*Copy_ppstrList)->Prev = Copy_pstrNode;
+          *Copy_ppstrList = Copy_pstrNode;
+        }/* if */
+        else
+        {
+          while((Loc_pstrTraverser->Next != NULL) && (Loc_pstrTraverser->Next->WaitTicks < Copy_pstrNode->WaitTicks))
+          {
+            Loc_pstrTraverser = Loc_pstrTraverser->Next;
+          }/* while */
+          Copy_pstrNode->Next = Loc_pstrTraverser->Next;
+          Copy_pstrNode->Prev = Loc_pstrTraverser;
+          Loc_pstrTraverser->Next = Copy_pstrNode;
+          if(Copy_pstrNode->Next != NULL)
+          {
+            Copy_pstrNode->Next->Prev = Copy_pstrNode;
+          }/* if */
+        }/* else */
+      }/* else if */
+      else
+      {
+        /* MISRA */
+      }/* else */
+    }/* else */
+  }/* if */
 }/* List_vidInsert */
 strTCB_t* List_pstrExtractHead(strTCB_t** Copy_ppstrList)
 {
@@ -23,14 +87,14 @@ strTCB_t* List_pstrExtractHead(strTCB_t** Copy_ppstrList)
   if((Copy_ppstrList != NULL) && (*Copy_ppstrList != NULL))
   {
     Loc_pstrHead = *Copy_ppstrList;
-    *Copy_ppstrList = *Copy_ppstrList->Next;
+    *Copy_ppstrList = (*Copy_ppstrList)->Next;
     Loc_pstrHead->Next = NULL;
     if(*Copy_ppstrList != NULL)
     {
-      *Copy_ppstrList->Prev = NULL;
+      (*Copy_ppstrList)->Prev = NULL;
     }/* if */
   }/* if */
 
   return(Loc_pstrHead);
 }/* List_pstrExtractHead */
-/* ////////////////////////////////////////////////////////////////////// */
+/* ///////////////////////////////////////////////////////////////
--- 03_APP/02_Tasks/07_OS_Test/OS_Test.c
+++ 03_APP/02_Tasks/07_OS_Test/OS_Test.c
@@ -10,19 +10,46 @@
 #include "StdTypes.h"
 /* OS */
 #include "Core.h"
+#include "List.h"
 /* MCAL */
 /* HAL */
 #include "Led.h"
 /* Tracing */
 #include "diag/Trace.h"
 /* /////////////////////////////////////////////////////////////////// */
+extern strTCB_t* OS_pstrReadyList  ;
+extern strTCB_t* OS_pstrWaitingList;
 
 /* /////////////////////////// Entry Point ////////////////////// */
 int main(void)
 {
 
-  OS_vidCreateTask()
-  OS_vidStart();
+//  OS_vidCreateTask()
+//  OS_vidStart();
+  strTCB_t TCB1 = {NULL,NULL,NULL,NULL,NULL,1,100};
+  strTCB_t TCB2 = {NULL,NULL,NULL,NULL,NULL,2,200};
+  strTCB_t TCB3 = {NULL,NULL,NULL,NULL,NULL,3,300};
+  strTCB_t TCB4 = {NULL,NULL,NULL,NULL,NULL,4,400};
+
+  List_vidInsert(&OS_pstrReadyList, &TCB3, enuOrderType_Pri);
+  List_vidInsert(&OS_pstrReadyList, &TCB2, enuOrderType_Pri);
+  List_vidInsert(&OS_pstrReadyList, &TCB4, enuOrderType_Pri);
+  List_vidInsert(&OS_pstrReadyList, &TCB1, enuOrderType_Pri);
+
+  List_pstrExtractHead(&OS_pstrReadyList); 
+  List_pstrExtractHead(&OS_pstrReadyList); 
+  List_pstrExtractHead(&OS_pstrReadyList); 
+  List_pstrExtractHead(&OS_pstrReadyList); 
+
+  List_vidInsert(&OS_pstrWaitingList, &TCB3, enuOrderType_Tick);
+  List_vidInsert(&OS_pstrWaitingList, &TCB2, enuOrderType_Tick);
+  List_vidInsert(&OS_pstrWaitingList, &TCB4, enuOrderType_Tick);
+  List_vidInsert(&OS_pstrWaitingList, &TCB1, enuOrderType_Tick);
+
+  List_pstrExtractHead(&OS_pstrWaitingList); 
+  List_pstrExtractHead(&OS_pstrWaitingList); 
+  List_pstrExtractHead(&OS_pstrWaitingList); 
+  List_pstrExtractHead(&OS_pstrWaitingList); 
   while(1)
   {
 
